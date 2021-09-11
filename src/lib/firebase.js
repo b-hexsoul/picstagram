8739c9d7882d119b8e36552e551053552a1b83cf
b-hexsoul <brandon.hexsel@gmail.com>
2021-09-11 08:38:34 -0700

    'Firebase config'

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,16 @@
-import Firebase from 'firebase';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { seedDatabase } from '../seed';
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-const config = {
+const firebase = Firebase.initializeApp({
+  apiKey: 'AIzaSyDr5kZnaLJxdlm7euiycYHej_7yLXR-Wbo',
+  authDomain: 'picstagram-hex.firebaseapp.com',
+  projectId: 'picstagram-hex',
+  storageBucket: 'picstagram-hex.appspot.com',
+  messagingSenderId: '657566686876',
+  appId: '1:657566686876:web:cab2a44b8c96fd82321759'
+});
 
-}
-
-const firebase = Firebase.initializeApp(config)
 const { FieldValue } = Firebase.firestore;
 
-seedDatabase(firebase)
-
-export {firebase, FieldValue}
+export { firebase, FieldValue };
