83b250e2484193be129ea7f468e33d507dbb4b7b
b-hexsoul <brandon.hexsel@gmail.com>
2021-09-14 05:12:06 -0700

    'Update syntax'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import FirebaseContext from './context/firebase'
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
 
 ReactDOM.render(
-  <FirebaseContext.Provider value={ firebase, FieldValue}>
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
   </FirebaseContext.Provider>,
   document.getElementById('root')
