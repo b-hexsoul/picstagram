1f611d48b7eb15a102bf7d8c4c3d05ca68355088
b-hexsoul <brandon.hexsel@gmail.com>
2021-09-14 05:12:15 -0700

    'Add colors'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        white: '#ffffff',
+        purple: {
+          primary: '#6C508B',
+          secondary: '#AB7AE2',
+          light: '#e8c6ff'
+        },
+        red: {
+          primary: '#ed4956'
+        },
+        blue: {
+          medium: '#005c98'
+        },
+        gray: {
+          base: '#616161',
+          primary: '#dbdbdb',
+          background: '#fafafa'
+        },
+        black: {
+          light: '#005c98',
+          faded: '#000059'
+        }
+      }
+    }
   },
   variants: {
     extend: {}
