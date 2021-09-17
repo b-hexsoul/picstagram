463f4389993d04f3cb7b84333189acc141b11fbf
b-hexsoul <brandon.hexsel@gmail.com>
2021-09-17 02:37:53 -0700

    'reorganize routes'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import useAuthListener from './hooks/use-auth-listener';
 import UserContext from './context/user';
 import * as ROUTES from './constants/routes';
-const Dashbaord = lazy(() => import('./pages/dashboard'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 const Login = lazy(() => import('./pages/login'));
 const Signup = lazy(() => import('./pages/sign-up'));
 const NotFound = lazy(() => import('./pages/not-found'));
@@ -16,9 +16,9 @@ function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.DASHBOARD} component={Dashbaord} />
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={Signup} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
             <Route component={NotFound} />
           </Switch>
         </Suspense>
